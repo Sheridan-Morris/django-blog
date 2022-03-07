3a4d4a6d31d41f7eefc97db1f596faed24e823f1
Sheridan-Morris <68619585+Sheridan-Morris@users.noreply.github.com>
2022-03-06 22:01:19 -0500

    Refactor views to class-based views.

--- blogging/views.py
+++ blogging/views.py
@@ -1,14 +1,22 @@
+from typing import List
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
-
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 def list_view(request):
     published = Post.objects.exclude(published_date__exact = None)
     posts = published.order_by('-published_date')
     context = {'posts': posts }
     return render(request, 'blogging/list.html', context)
+
+class BloggingListView(ListView):
+    model = Post
+    # queryset = model.objects.filter(published_date__exact != None)
+    queryset = model.objects.order_by('-published_date').exclude(published_date = None)
+    template_name = 'blogging/list.html'
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
