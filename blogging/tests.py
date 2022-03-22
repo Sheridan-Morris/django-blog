50b1cc1f9b2f9abb045fbfc3a17f95a7ceace27b
Sheridan-Morris <68619585+Sheridan-Morris@users.noreply.github.com>
2022-03-22 10:04:59 -0400

    Apply Black.

--- blogging/tests.py
+++ blogging/tests.py
@@ -5,8 +5,11 @@ from django.contrib.auth.models import User
 from blogging.models import Post, Category
 from django.utils.timezone import utc
 
+
 class PostTestCase(TestCase):
-    fixtures = ['blogging_test_fixture.json', ]
+    fixtures = [
+        "blogging_test_fixture.json",
+    ]
 
     def setUp(self):
         self.user = User.objects.get(pk=1)
@@ -17,36 +20,37 @@ class PostTestCase(TestCase):
         actual = str(p1)
         self.assertEqual(expected, actual)
 
-class CategoryTestCase(TestCase):
 
+class CategoryTestCase(TestCase):
     def test_string_representation(self):
-        expected = 'A Category'
-        c1 = Category(name = expected)
+        expected = "A Category"
+        c1 = Category(name=expected)
         actual = str(c1)
         self.assertEqual(expected, actual)
 
+
 class FrontEndTestCase(TestCase):
-    '''
+    """
     test views for front end
-    '''
+    """
 
     fixtures = [
-        'blogging_test_fixture.json',
-        ]
-    
+        "blogging_test_fixture.json",
+    ]
+
     def setUp(self):
         self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
         self.timedelta = datetime.timedelta(15)
         author = User.objects.get(pk=1)
-        for count in range(1,11):
-            post = Post(title='Post %d Title' % count, text='foo', author=author)
+        for count in range(1, 11):
+            post = Post(title="Post %d Title" % count, text="foo", author=author)
             if count < 6:
                 pubdate = self.now - self.timedelta * count
                 post.published_date = pubdate
             post.save()
 
     def test_list_only_published(self):
-        resp = self.client.get('/')
+        resp = self.client.get("/")
         resp_text = resp.content.decode(resp.charset)
         self.assertTrue("My Cool Blog Posts" in resp_text)
         for count in range(1, 11):
@@ -58,10 +62,12 @@ class FrontEndTestCase(TestCase):
 
     def test_details_only_published(self):
         for count in range(1, 11):
-            title = 'Post %d Title' % count
-            post = Post.objects.get(title = title)
-            resp = self.client.get('/posts/%d/' % post.pk)
-            if count < 12:  # TODO: Why was this listed as if count < 6 originally?  Shouldn't this always return 200 as written?
+            title = "Post %d Title" % count
+            post = Post.objects.get(title=title)
+            resp = self.client.get("/posts/%d/" % post.pk)
+            if (
+                count < 12
+            ):  # TODO: Why was this listed as if count < 6 originally?  Shouldn't this always return 200 as written?
                 self.assertEqual(resp.status_code, 200)
                 self.assertContains(resp, title)
             else:
